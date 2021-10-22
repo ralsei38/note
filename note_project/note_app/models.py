238dd9eeb89dad556c8b9fc32892f15487fdc97a
ralsei38 <fontana.theo.me@gmail.com>
2021-10-22 16:45:22 +0200

    modif attribute name

--- note_project/note_app/models.py
+++ note_project/note_app/models.py
@@ -4,5 +4,5 @@ from django.db.models.base import Model
 # Create your models here.
 class Note(models.Model):
     title = models.CharField(max_length=40)
-    text = models.CharField(max_length=200)
+    desc = models.CharField(max_length=200)
     is_done = models.BooleanField(default=False)

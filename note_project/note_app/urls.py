abe03bd986553be46149ca63640e7c6bb3b9f3aa
ralsei38 <fontana.theo.me@gmail.com>
2021-10-24 21:00:20 +0200

    form handling form's data processing index path static -> dynamic notes migrations

--- note_project/note_app/urls.py
+++ note_project/note_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('form', views.form, name='form'),
+    path('add', views.add, name='add'),
+    path('set_as_done', views.set_as_done, name='set_as_done')
 ]
--- note_project/note_app/views.py
+++ note_project/note_app/views.py
@@ -1,12 +1,37 @@
-from django.shortcuts import render
+# from django.http.response import Http404
 # from django.http import HttpResponse
 # from django.template import loader
 from django.shortcuts import render
+from .forms import NoteForm
+from .models import Note
+
 # Create your views here.
 def index(request):
-    context = {}
+    notes = Note.objects.all()
+    context = {'notes': notes}
     return render(request, 'index.html', context)
 
 def form(request):
     context = {}
-    return render(request, 'form.html', context)
+
+    if request.method == 'GET':
+        form = NoteForm(request.GET)
+
+        if form.is_valid():
+            #insert data in db
+            #return index page
+            form.save()
+            return index(request)
+        else:
+            #return blank form
+            context = {'form': NoteForm()}
+            return render(request, 'form.html', context)
+    else:
+        raise 'help'
+
+def add(request):
+    context = {}
+    return render(request, 'index.html', context)
+
+def set_as_done(request):
+    return index(request)

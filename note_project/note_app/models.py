e713aab095ef61fc283b2a055e4b27db6cbd5ca9
ralsei38 <fontana.theo.me@gmail.com>
2021-10-24 20:55:54 +0200

    str cast for the note class

--- note_project/note_app/models.py
+++ note_project/note_app/models.py
@@ -6,3 +6,6 @@ class Note(models.Model):
     title = models.CharField(max_length=40)
     desc = models.CharField(max_length=200)
     is_done = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return "title: {}\ndescription: {}\n".format(self.title, self.desc)

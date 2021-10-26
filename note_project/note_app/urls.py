fb940ab6307935ef2b9adf180d40352101fc5cab
ralsei38 <fontana.theo.me@gmail.com>
2021-10-26 22:35:22 +0200

    'done' page ctrl function and route

--- note_project/note_app/urls.py
+++ note_project/note_app/urls.py
@@ -4,5 +4,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('form', views.form, name='form'),
     path('add', views.add, name='add'),
-    path('set_as_done', views.set_as_done, name='set_as_done')
+    path('set_as_done/<int:id>', views.set_as_done, name='set_as_done'),
+    path('delete/<int:id>', views.delete, name='delete'),
+    path('done', views.done, name='done')
 ]
--- note_project/note_app/views.py
+++ note_project/note_app/views.py
@@ -1,15 +1,21 @@
 # from django.http.response import Http404
 # from django.http import HttpResponse
 # from django.template import loader
-from django.shortcuts import render
-from .forms import NoteForm
+from django.http.response import Http404
+from django.shortcuts import redirect, render
+from .forms import NoteForm, DoneForm
 from .models import Note
 
 # Create your views here.
 def index(request):
-    notes = Note.objects.all()
+    notes = Note.objects.filter(is_done=0)
     context = {'notes': notes}
     return render(request, 'index.html', context)
+
+def done(request):
+    notes = Note.objects.filter(is_done=1)
+    context = {'notes': notes}
+    return render(request, 'done.html', context)
 
 def form(request):
     context = {}
@@ -21,7 +27,7 @@ def form(request):
             #insert data in db
             #return index page
             form.save()
-            return index(request)
+            return redirect('index')
         else:
             #return blank form
             context = {'form': NoteForm()}
@@ -33,5 +39,21 @@ def add(request):
     context = {}
     return render(request, 'index.html', context)
 
-def set_as_done(request):
-    return index(request)
+def delete(request, id):
+    if request.method == 'GET':
+        note = Note.objects.filter(pk=id)
+        note.delete()
+    else:
+        Http404()
+    return redirect('done')
+
+
+def set_as_done(request, id):
+    if request.method == 'GET':
+        note = Note.objects.filter(pk=id)[0]
+        note.is_done = 1
+        note.save()
+        print(note)
+    else:
+        Http404()
+    return redirect('index')

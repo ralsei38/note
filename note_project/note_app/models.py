6f65a3926b1faf47af9f3a13984b5da3eb8dfae1
ralsei38 <fontana.theo.me@gmail.com>
2021-10-29 21:29:19 +0200

    migrations, too early for categories

--- note_project/note_app/models.py
+++ note_project/note_app/models.py
@@ -10,10 +10,10 @@ class Category(models.Model):
         return "title: {}\ndescription: {}\n".format(self.title, self.desc)
 
 class Note(models.Model):
-    title = models.CharField(max_length=40)
-    desc = models.CharField(max_length=200)
+    title = models.CharField(default='', max_length=40)
+    desc = models.CharField(default='', max_length=200)
     is_done = models.BooleanField(default=False)
-    category = models.ForeignKey(Category, on_delete=CASCADE)
+    # category = models.ForeignKey(Category, on_delete=CASCADE)
 
     def __str__(self) -> str:
         return "title: {}\ndescription: {}\n".format(self.title, self.desc)
